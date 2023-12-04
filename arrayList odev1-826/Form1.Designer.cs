bab2c3c04993c490914b0fc58d22de9208746cc2
kubrasayitass <145451189+kubrasayitass@users.noreply.github.com>
2023-12-04 09:37:17 +0300

    Add files via upload

---
+++ arrayList odev1-826/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace arrayList_odev1_826
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblsehirAdi = new System.Windows.Forms.Label();
+            this.txtSehirAdi = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblsehirAdi
+            // 
+            this.lblsehirAdi.AutoSize = true;
+            this.lblsehirAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblsehirAdi.Location = new System.Drawing.Point(43, 89);
+            this.lblsehirAdi.Name = "lblsehirAdi";
+            this.lblsehirAdi.Size = new System.Drawing.Size(70, 18);
+            this.lblsehirAdi.TabIndex = 0;
+            this.lblsehirAdi.Text = "Şehir Adı:";
+            // 
+            // txtSehirAdi
+            // 
+            this.txtSehirAdi.Location = new System.Drawing.Point(130, 87);
+            this.txtSehirAdi.Name = "txtSehirAdi";
+            this.txtSehirAdi.Size = new System.Drawing.Size(100, 20);
+            this.txtSehirAdi.TabIndex = 1;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(255, 85);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(94, 23);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtSehirAdi);
+            this.Controls.Add(this.lblsehirAdi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblsehirAdi;
+        private System.Windows.Forms.TextBox txtSehirAdi;
+        private System.Windows.Forms.Button btnEkle;
+    }
+}
+
---
+++ arrayList odev1-826/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arrayList_odev1_826
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ArrayList sehirler = new ArrayList();
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            sehirler.Add(txtSehirAdi.Text);
+        }
+    }
+}
